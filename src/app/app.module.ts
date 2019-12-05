ceadfddab64c248d1c957c5eefd5aedc1928b98a
Dresoy Ravelo <dr@target-traffic.net>
2019-12-05 10:55:55 -0300

    Added the git submodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { App1SharedModule } from 'subapp1-test';
+import { App1SharedModule } from 'projects/subapp1';
+// import { App1SharedModule } from 'subapp1-test';
 
 @NgModule({
   declarations: [
